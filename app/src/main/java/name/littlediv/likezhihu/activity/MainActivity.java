1386833242bece5b67a4e9623673945f3aaeaf88
littlediv <1299774687@qq.com>
2016-06-12 11:41:45 +0800

    init

--- app/src/main/java/name/littlediv/likezhihu/activity/MainActivity.java
+++ app/src/main/java/name/littlediv/likezhihu/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package name.littlediv.likezhihu;
+package name.littlediv.likezhihu.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import name.littlediv.likezhihu.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
